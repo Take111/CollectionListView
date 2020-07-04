e6b9b27c7ed8a2ec5021c4867774a4dee08886d6
Take111 <maxmaxastea@gmail.com>
2020-07-04 15:50:05 +0900

    Initial Commit

---
+++ ListCollectionViewTests/ListCollectionViewTests.swift
@@ -0,0 +1,33 @@
+//
+//  ListCollectionViewTests.swift
+//  ListCollectionViewTests
+//
+//  Created by 竹ノ内愛斗 on 2020/07/04.
+//
+
+import XCTest
+@testable import ListCollectionView
+
+class ListCollectionViewTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
